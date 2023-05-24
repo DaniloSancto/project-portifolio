f64bd8c03b2f1063f3462d43e39ed17226cf4537
Danilo Santos <danilo.santos0404@hotmail.com>
2023-05-24 11:47:48 -0300

    JwtService, generate token and verify if is expired or not. jsonwebtoken dependencies

--- src/main/java/dev/danilosantos/portifolio/config/JwtAuthenticationFilter.java
+++ src/main/java/dev/danilosantos/portifolio/config/JwtAuthenticationFilter.java
@@ -2,10 +2,12 @@ package dev.danilosantos.portifolio.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import dev.danilosantos.portifolio.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,12 +15,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private JwtService jwtService;
 
 	@Override
-	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request,
+			@NonNull HttpServletResponse response,
+			@NonNull FilterChain filterChain
+			) throws ServletException, IOException {
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String userEmail;
+		if (authHeader == null || authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		jwt = authHeader.substring(7);
+		userEmail = jwtService.extractUsername(jwt);
 	}
 
 }
