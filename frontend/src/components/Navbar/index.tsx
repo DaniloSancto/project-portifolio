fa367e4514d3af2c10b1b3f8ca0dd4130b84258a
Danilo Santos <danilo.santos0404@hotmail.com>
2023-07-18 16:48:08 -0300

    frontend: Navbar styled

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="nav-content">
+        <div className="nav-logo">
+          <a href="">
+            <h1>Portifólio</h1>
+          </a>
+        </div>
+        <div className="nav-links">
+          <a href="">
+            <p>LOGIN</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
